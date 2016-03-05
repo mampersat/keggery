1e52f97d4b2123025d5a89c24174179d7f6419e8
M@ <mSheppard@dyn.com>
2016-03-05 17:13:52 -0500

    exempt string in beer descrption will filter from list

--- OrderingCalcs.py
+++ OrderingCalcs.py
@@ -10,12 +10,6 @@ import json
 from datetime import datetime
 import numpy
 
-#List of keg_id's that we don't want to report on
-#Typically becuase they are not from an easy to acquire vendor
-exempt=[41, #northcountry hard cider
-        7, 6, #throwback
-        ]
-
 with open('kegs.json') as data_file:
     dict = json.load(data_file)
 
@@ -24,10 +18,11 @@ stats = {}
 #Drive through list collecting stats
 for keg in dict['objects']:
 
-    # filter out certain kegs
-    if keg['beverage']['id'] in exempt:
+    # filter out certain kegs (look for 'exempt' in description)
+    description = keg['beverage'].get('description','')
+    if description.lower().find('dynexempt')>=0:
         break
-        
+    
     name = "%s %s id=%s" % (keg['beverage']['producer']['name'], keg['beverage']['name'], keg['beverage']['id'])
   
     volume = keg['full_volume_ml']
@@ -42,7 +37,7 @@ for keg in dict['objects']:
         rate_l_day = rate_ml_s * 86.4
         rate_l_week = rate_l_day * 7
  
-        if 1 < rate_l_week < 1000: #sanity check
+        if 1 < rate_l_week < 10000: #sanity check
             
             if name not in stats:
                 stats[name] = {}
@@ -67,6 +62,6 @@ for w in sorted(stats, key=lambda rate:stats[rate]['rate'], reverse=True):
     s = "{:3.0f} liters/wk {:3d} days ago, {}\n".format(
         stats[w]['rate'], (datetime.today() -stats[w]['date']).days, w)
     f.write(s)
-    # print s,
+    print s,
     
 f.close()
